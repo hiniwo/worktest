504f62596e1c945772e14e7701016d85bcf77ae1
walker <hiniwoo@163.com>
2019-04-21 10:41:14 +0800

    class practice

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="home">
       <div>
-          <p>我是home页面</p>
+          <p>我是baidu页面</p>
       </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import HHcommon from './js/common'
 
 export default {
-  name: 'home'
+	data(){
+		return{
+		
+		}
+	},
+	mounted() {
+		var common  = new HHcommon('libai' , 'name');
+		console.log(common.toString())
+	}
 }
 </script>
